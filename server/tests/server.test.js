d71def8796cb9554781a82998f1fb94f2a0dec73
Sarp Solakoglu <ssolakoglu@zendesk.com>
2018-01-07 21:57:02 +0000

    added PATCH, DELETE endpoint and tests, added config

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,9 @@ const todos = [{
     text: 'Fist test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 123
 }];
 
 beforeEach((done) => {
@@ -93,6 +95,77 @@ describe('GET /todos/:id', () => {
             .get('/todos/123')
             .then((response) => {
                 expect(response.status).toBe(404);
+                done()
+            })
+            .catch(e => done(e));
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        request(app)
+            .delete(`/todos/${todos[0]._id.toHexString()}`)
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body.todo.text).toBe(todos[0].text);
+                done();
+            })
+            .catch(e => done(e));
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .then((response) => {
+                expect(response.status).toBe(404);
+                done()
+            })
+            .catch(e => done(e));
+    });
+
+    it('should return 404 if invalid obj id', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .then((response) => {
+                expect(response.status).toBe(404);
+                done()
+            })
+            .catch(e => done(e));
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update a todo', (done) => {
+        var text = 'New text';
+        request(app)
+            .patch(`/todos/${todos[0]._id.toHexString()}`)
+            .send({
+                completed: true,
+                text
+            })
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body.todo.text).toBe(text);
+                expect(response.body.todo.completed).toBe(true);
+                expect(typeof response.body.todo.completedAt).toBe('number');
+                done();
+            })
+            .catch(e => done(e));
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var text = 'New text';
+        request(app)
+            .patch(`/todos/${todos[0]._id.toHexString()}`)
+            .send({
+                completed: false,
+                text
+            })
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body.todo.text).toBe(text);
+                expect(response.body.todo.completed).toBeFalsy();
+                expect(response.body.todo.completedAt).toBeNull();
                 done()
             })
             .catch(e => done(e));
