fb5fa3c475f3256c9ff32cba4844c3106ec5dead
Sarp Solakoglu <ssolakoglu@zendesk.com>
2018-01-16 19:30:05 +0000

    Added DELETE /users/me/token and tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -276,3 +276,20 @@ describe('POST /users/login', () => {
             .catch(e => done(e));
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .then((response) => {
+                expect(response.status).toBe(200);
+                return User.findById(users[1]._id)
+            })
+            .then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            })
+            .catch(e => done(e));
+    });
+})
